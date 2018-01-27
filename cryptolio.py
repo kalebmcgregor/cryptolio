6ecdd253c4cbe0800416222304d265bd5bbba137
Kaleb McGregor <kaleb.mcgregor@gmail.com>
2018-01-26 19:00:19 -0800

    refactored variables in cryptolio.py and beefed the coin class with more variables

--- cryptolio.py
+++ cryptolio.py
@@ -1,23 +1,22 @@
-import requests
-import json
 import yaml
 
-import coin
+from coin import Coin
+
+
+def roi(gain, cost):
+    return (gain - cost)/cost
 
 
 with open("wallet.yml") as wallet_file:
     doc = yaml.load(wallet_file)
-    portfolio_total_usd = 0
-    purchase_total_usd = 0
+    portfolio_balance_usd = 0
+    purchase_balance_usd = 0
     for x in doc['coins']:
-        cur_coin = coin.Coin(x['id'], x['total'], x['purchase_usd'])
-        response = requests.get('https://api.coinmarketcap.com/v1/ticker/%s' % cur_coin.id)
-        coin_stats = json.loads(response.text)
-        coin_usd_total = cur_coin.total * float(coin_stats[0]['price_usd'])
-        portfolio_total_usd += coin_usd_total
-        purchase_total_usd += cur_coin.purchase_usd
-        coin_roi = (1 - cur_coin.purchase_usd / coin_usd_total) * 100
-        print("$%.2f %s, %.2f%% roi, %f total" % (coin_usd_total, cur_coin.id, coin_roi, cur_coin.total))
-    total_roi = (1 - purchase_total_usd / portfolio_total_usd) * 100
-    total_profit = portfolio_total_usd - purchase_total_usd
-    print("$%.2f grand total, %.2f%% total roi, $%.2f profit" % (portfolio_total_usd, total_roi, total_profit))
+        coin = Coin(x['id'], x['total'], x['purchase_usd'])
+        portfolio_balance_usd += coin.balance
+        purchase_balance_usd += coin.purchase_usd
+        print("$%.2f %s, %.2f%% roi, %f total" % (coin.balance, coin.id,
+                                                  roi(coin.gain, coin.purchase_usd), coin.total))
+    total_roi = (1 - purchase_balance_usd / portfolio_balance_usd) * 100
+    total_profit = portfolio_balance_usd - purchase_balance_usd
+    print("$%.2f grand total, %.2f%% total roi, $%.2f profit" % (portfolio_balance_usd, total_roi, total_profit))
