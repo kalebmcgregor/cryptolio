8e3b0338e75ac5c16ac28e73f5902a9f06179524
Kaleb McGregor <kaleb.mcgregor@gmail.com>
2018-02-05 11:30:47 -0800

    Updating requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 install_requires = [
     'pyyaml==3.12',
-    'requests==2.18.x'
+    'requests==2.18.4'
 ]
 
 setup(
     name='cryptolio',
     version='1.0.0',
-    packages=['cryptolio', 'cryptolio.test'],
+    packages=['cryptolio'],
     url='',
     license='GPL-3.0',
     author='Kaleb McGregor',
